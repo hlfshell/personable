ec0f138afa5c4a259d862e09626ccfa88e40445a
Keith Chester <kchester@gmail.com>
2018-07-15 18:35:56 -0500

    feature: setup.py for publishing and packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='personable',
+      version='0.1',
+      description='Frame by frame person recognition and tracking',
+      url='https://github.com/hlfshell/personable',
+      author='Keith Chester',
+      author_email='kchester@gmail.com',
+      license='MIT',
+      packages=['personable'],
+      zip_safe=False)
